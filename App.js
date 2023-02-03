03f9cce1af5500f8356c4d1a21f4c14dd6252764
jayshah <jaywebdev98@gmai.com>
2023-02-03 19:59:27 +0530

    setup tailwind css

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const App = () => {
+  return (
+    <>
+      <h1 className="text-[50px]">Hello From Parcel</h1>
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<h1>Hello From Parcel</h1>);
+root.render(<App />);
